a5838603b0b4d575eba73017d5590090107bb0b1
Student GOU SPO VGPEK <Student@corp.vgpek.ru>
2025-10-24 10:21:18 +0300

    Message page created

--- obj/Debug/net8.0-windows/MixedMessagesClient.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MixedMessagesClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MixedMessagesClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef612436047b016733d90a4e3bfbc7459da27c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7aa4f46997d02be831a9e81252dcd3765c2bc7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MixedMessagesClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MixedMessagesClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
